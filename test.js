cdbcabfbec0714526fd45afae054ea472db92961
Anaboy01 <aliyuanate016@gmail.com>
2023-03-02 15:17:02 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,23 @@
+// math method is an intrinsic object that provide basic mathmathics functionalities and constant 
+// let x = 3.14 
+// x = math.round(x)
+// x = Math.floor(x)
+// x = Math.ceil(x)
+// x = Math.pow(x, 2)
+// x = Math.sqrt(x)
+// x = Math.abs(x)
+// x = Math.max(x)
+// x = Math.min(x)
+// console.log(x)
+
+// let x = 3.19;
+// let y = 5;
+// let z = 9
+
+// maximum = Math.max(x,y,z)
+// console.log(maximum)
+// minimum = Math.min(x,y,z)
+// console.log(minimum)
+// const resetBtn = document.querySelector('#resetBtn')
+
+// resetBtn.addEventListener('click', () => window. ())
